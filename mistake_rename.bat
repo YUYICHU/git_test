a4a4973fe58b55b2e5ffe4af3acf89ac3beb279d
BAMBOO-NB\bamboo <mean4sleeping@gmail.com>
2018-09-11 17:26:21 +0800

    test1

---
+++ mistake_rename.bat
@@ -0,0 +1,21 @@
+ren SW4_Shawn.png 1.png
+ren TR1_Shawn.png 2.png
+ren TR2_Shawn.png 3.png
+ren TR3_Shawn.png 4.png
+ren TR4_Shawn.png 5.png
+ren YU1_Shawn.png 6.png
+ren YU2_Shawn.png 7.png
+ren YU3_Shawn.png 8.png
+ren YU4_Shawn.png 9.png
+del YU5_Shawn.png
+timeout /t 1
+ren 1.png TR1_Shawn.png
+ren 2.png TR2_Shawn.png
+ren 3.png TR3_Shawn.png
+ren 4.png TR4_Shawn.png
+ren 5.png YU1_Shawn.png
+ren 6.png YU2_Shawn.png
+ren 7.png YU3_Shawn.png
+ren 8.png YU4_Shawn.png
+ren 9.png YU5_Shawn.png
+
